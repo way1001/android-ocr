8434f4022eceab59214aed73cd7eb4cb94d59e3e
zhuxibrian <zhuxibrian@icloud.com>
2018-12-14 16:54:41 +0800

    添加菜单

--- app/src/main/java/com/rw/zx/androidocr/MainActivity.java
+++ app/src/main/java/com/rw/zx/androidocr/MainActivity.java
@@ -2,6 +2,7 @@ package com.rw.zx.androidocr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //这里是在登录界面label上右上角添加三个点，里面可添加其他功能
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.mainMenu, menu);//这里是调用menu文件夹中的main.xml，在登陆界面label右上角的三角里显示其他功能
+        return true;
     }
 }
